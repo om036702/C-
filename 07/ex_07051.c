59a1cd3289c5729c77786bfa2dd722f19c7574d6
om036702 <139424475+om036702@users.noreply.github.com>
2024-04-23 14:45:09 +0900

    Update ex_07051.c

--- 07/ex_07051.c
+++ 07/ex_07051.c
@@ -25,7 +25,13 @@ int main(void)
 
   c = b % a;
   printf("b %% a  = %d\n", c);
-
+  
+/*
+int型の値とint型の値の算術演算を行うとその結果はint型になり、double型の値とdouble型の値の算術演算を行うとその結果はdouble型になります。
+int型の値とdouble型の値の算術演算を行うと、int型の値がdouble型の値に暗黙的に変換され、その結果はdouble型になります。
+注意が必要なのは、int型の値をint型の値で割ったときです。
+*/
+  
   double d;
   d = 3 / 2;
   printf("1) d = %f\n", d);
