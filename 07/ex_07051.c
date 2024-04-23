9303052609bf8f9cea9ca37ba8058e368c90f1f0
om036702 <139424475+om036702@users.noreply.github.com>
2024-04-23 14:42:25 +0900

    Create ex_07051.c

---
+++ 07/ex_07051.c
@@ -0,0 +1,35 @@
+/*
+5.	演算子
+*/
+
+#include <stdio.h>
+int main(void)
+{
+  int a = 3;
+  int b = 5;
+  int c;
+  
+  c = a + b;
+  printf("a + b = %d\n", c);
+
+  c = a - b;
+  printf("a - b = %d\n", c);
+
+  c = a * b;
+  printf("a * b = %d\n", c);
+
+  c = b / a;
+  printf("b / a  = %d\n", c);
+
+  c = b % a;
+  printf("b %% a  = %d\n", c);
+
+  double d;
+  d = 3 / 2;
+  printf("1) d = %f\n", d);
+
+  d = 3.0 / 2;
+  printf("2) d = %f\n", d);
+
+  return 0;
+}
