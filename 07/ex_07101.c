550ade17cd8dc79a98a45523f33c38b3db4b0df1
om036702 <139424475+om036702@users.noreply.github.com>
2024-04-23 15:15:02 +0900

    Create ex_07101.c

---
+++ 07/ex_07101.c
@@ -0,0 +1,19 @@
+/*
+--------------------------------------------------
+練習問題 10- 1
+--------------------------------------------------
+*/
+#include <stdio.h>
+
+int main(void){
+    int x = 21;
+    int y = 10;
+    printf("x: %d y: %d\n",x,y);
+
+    printf("x + y = : %d\n",x + y);
+    printf("x - y = : %d\n",x - y);
+    printf("x * y = : %d\n",x * y);
+    printf("x / y = : %d  x %% y = %d\n",x / y, x % y);
+
+    return 0;
+}
