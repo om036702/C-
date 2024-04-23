c8af23340d3c927d87b574211cb4c9ad666e7da6
om036702 <139424475+om036702@users.noreply.github.com>
2024-04-23 15:27:49 +0900

    Update ex_07102.c

--- 07/ex_07102.c
+++ 07/ex_07102.c
@@ -5,12 +5,12 @@
 */
 #include <stdio.h>
 int main(void){
-	int a,b;
-	a = 1;
-	b = ++a;
-	printf("a=%d\tb=%d\n", a, b);
-	a = 1;
-	b = a++;
-	printf("a=%d\tb=%d\n", a, b);
-return 0;
+  int a,b;
+  a = 1;
+  b = ++a;
+  printf("a=%d\tb=%d\n", a, b);
+  a = 1;
+  b = a++;
+  printf("a=%d\tb=%d\n", a, b);
+  return 0;
 }
