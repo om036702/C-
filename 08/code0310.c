9e916dc53013eea796367858a3c9b084415b9413
om036702 <139424475+om036702@users.noreply.github.com>
2024-04-24 13:45:53 +0900

    Create code0310.c

---
+++ 08/code0310.c
@@ -0,0 +1,16 @@
+/*
+--------------------------------------------------
+ chap03   code0310.c
+--------------------------------------------------
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+  int age = 29;
+  printf("今年%d歳で、", age);
+  int newAge = age + 1;
+  printf("来年%d歳ですね\n", newAge);
+  return 0;
+}
